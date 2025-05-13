65dcc4a3727a135725c535b3403b8e6fe13d3bf8
Jakob Durstberger <jakob@durstberger.me>
2025-05-13 14:00:57 +0200

    logging

--- bump_version.sh
+++ bump_version.sh
@@ -1,4 +1,6 @@
 CURRENT_TAG="$(git tag --sort=committerdate | tail -1)"
 
+echo "setting current verion to $CURRENT_TAG"
 npm version $CURRENT_TAG --no-git-tag-version
+echo "bumping version to next minor version"
 npm version -f minor --no-git-tag-version
